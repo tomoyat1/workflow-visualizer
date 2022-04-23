b80aae7dd0d15802f3e88d89b83c0dd0381e4e58
Tomoya Tabuchi <t@tomoyat1.com>
2022-04-24 00:36:55 +0900

    Copy Visx network example

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App width={640} height={400}/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
